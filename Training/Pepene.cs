69410ad759865f0bd68bc3be5e118ad98c43487f
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-09-18 16:58:10 +0300

    Added test methods

---
+++ Training/Pepene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training
+{
+    [TestClass]
+    public class Pepene
+    {
+        [TestMethod]
+        public void ValoareNegativa()
+        {
+            Assert.AreEqual("Invalid", CanBeSplit(-2));
+        }
+
+        [TestMethod]
+        public void Par()
+        {
+            Assert.AreEqual("DA", CanBeSplit(10));
+        }
+
+        [TestMethod]
+        public void Impar()
+        {
+            Assert.AreEqual("NU", CanBeSplit(9));
+        }
+
+        private string CanBeSplit(int greutate)
+        {
+            if (greutate <= 0)
+                return "Invalid";
+            return (greutate % 2 == 0) ? "DA" : "NU";
+        }
+    }
+}
