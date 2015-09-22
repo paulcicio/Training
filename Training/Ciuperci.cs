39bf9aad936e2751ead73add93cb4e768d19d4f5
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-09-22 15:07:58 +0300

    Added 2 new items

---
+++ Training/Ciuperci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Training
+{
+    [TestClass]
+    public class Ciuperci
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {   List <int> list = Mushrooms(12, 3);
+            Assert.AreEqual(3,list[0]);
+            Assert.AreEqual(9, list[1]);
+        }
+
+        private List <int> Mushrooms(int n, int x)
+        {
+            List <int> list = new List <int> ();
+            int albe, rosii;
+            albe = n / (x + 1);
+            rosii = albe * x;
+            list.Add(albe);
+            list.Add(rosii);
+            return list;
+            
+        }
+    }
+}
---
+++ Training/Goat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training
+{
+    [TestClass]
+    public class Goat
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(60, Goats(10, 10, 20, 20, 15));
+        }
+
+        private double Goats(double x, double y, double z, double q, double w)
+        {
+            double daily = z / (x * y);
+            double hay = daily * q * w;
+            return hay; 
+        }
+    }
+}
