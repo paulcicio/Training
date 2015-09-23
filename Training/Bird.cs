af5699ece96726c20bf9f960824800d6217a9ba8
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-09-23 17:37:43 +0300

    Added Bird problem

---
+++ Training/Bird.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training
+{
+    [TestClass]
+    public class Bird
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(4, Distance(8, 2));
+        }
+
+        public int Distance(int distance, int speed)
+        {
+            int timeTrain = (distance / 2) / (2 * speed);
+            int distanceBird = timeTrain * (2 * speed);
+            return distanceBird;
+        }
+    }
+}
