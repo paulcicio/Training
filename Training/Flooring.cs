4683bf3f4b067412b22432092896e57e18b899ef
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-09-25 15:25:38 +0300

    Added Flooring problem

---
+++ Training/Flooring.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training
+{
+    [TestClass]
+    public class Flooring
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(6, Parquet(6, 5, 2, 3));
+        }
+
+        public int Parquet(int m, int n, int a, int b)
+        {
+            double area = m * n + 0.15 * (m * n);
+            double floor = Math.Ceiling(area / (a * b));
+            return Convert.ToInt32(floor);
+
+        }
+    }
+}
