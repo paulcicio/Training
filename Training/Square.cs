70aa3da80c3a2ef799038f753e67b5d025912add
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-09-25 13:57:08 +0300

    Added Square problem

---
+++ Training/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training
+{
+    [TestClass]
+    public class Square
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(4, Pavement (6, 6, 4));
+        }
+
+        public double Pavement (double m, double n, double a)
+        {
+            if (n <= a)
+                return Math.Ceiling(m / a);
+            else
+                return Math.Ceiling(m / a) * 2;
+        }
+    }
+}
