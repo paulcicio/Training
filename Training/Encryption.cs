eab563f8b0ed4fd52534ea79c06fab0c4e7a532f
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-10-02 12:49:18 +0300

    Added Lottery.cs and Encryption.cs

---
+++ Training/Encryption.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training
+{
+    [TestClass]
+    public class Encryption
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("neeaircsciaaanaxiucw", EncryptMessage("nicaieri nu e ca acasa", 4 ));
+        }
+
+        private string EncryptMessage(string message, int nrRows)
+        {
+            Console.WriteLine("Introduceti mesajul original: ");
+            string message1 = message.Replace(" ", "");
+            int nrChar = message1.Length;
+            int nrLines = (int) Math.Ceiling((double) nrChar / nrRows);
+            int nrRandomChar = (nrLines * nrRows) - nrChar;
+            Random r = new Random();
+            int num = r.Next(0, 26);
+            char[] litera = new char[nrRandomChar];
+            for (int i = 0; i < nrRandomChar; i++)
+            {
+                litera[i] = (char)('a' + num);
+            }
+
+            string[] line = new string[nrLines];
+            for (int i = 0; i < nrLines; i++)
+            {
+                for (int j=0;j<nrChar;j=j+nrLines)
+                    if (i + j < nrChar)
+                        line[i] += message1[i+j];
+            }
+
+            for (int i = nrLines-1; i > 0; i--)
+            {
+                if (nrRandomChar < 1)
+                    break;
+                else
+                    {
+                    line[i] += litera[nrRandomChar - 1];
+                    nrRandomChar--;
+                     }
+            }
+          
+            string rezultat = null;
+            for (int i = 0; i < nrLines; i++)
+                rezultat += line[i];
+            return rezultat;
+        }
+    }
+}
---
+++ Training/Lottery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Training
+{
+    [TestClass]
+    public class Lottery
+    {
+        [TestMethod]
+        public void FirstCategory()
+        {
+            Assert.AreEqual(0.00000007, WinTheLottery(6));        
+        }
+
+        [TestMethod]
+        public void SecondCategory()
+        {
+            Assert.AreEqual(0.00001845, WinTheLottery(5));
+        }
+
+        [TestMethod]
+        public void ThirdCategory()
+        {
+            Assert.AreEqual(0.00096862, WinTheLottery(4));
+        }
+
+        public double WinTheLottery(int n)
+        {
+            Console.WriteLine("Pentru categoriile I, II si III de castig, aveti urmatoarele sanse: ");
+            double probability = CalculateCombinations(6, n) * CalculateCombinations(43, 6 - n) / CalculateCombinations(49, 6);
+            return Math.Round (probability, 8); 
+           
+        }
+
+        public double CalculateCombinations(int n, int k)
+        {
+            return CalculateFactorial(n) / (CalculateFactorial(k) * CalculateFactorial(n - k));
+
+        }
+
+        public double CalculateFactorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            double factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial = factorial * i;
+            }
+            return factorial;
+        }
+            
+    }
+}
