b58e2f4a20db61bea40946c4e38e744998b78729
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-10-19 15:36:24 +0300

    Finished Football project

--- Football/Football/UnitTest1.cs
+++ Football/Football/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Football
 {
@@ -7,6 +8,7 @@ namespace Football
     public class UnitTest1
     {
         public Match[] round = { new Match("U Cluj", "CFR", 2, 1), new Match("Petrolul", "Steaua", 0, 0), new Match("Botosani", "CSU", 3, 2), new Match("Dinamo", "ASA", 2, 1), new Match("Chiajna", "Astra", 0, 2) };
+
 
         [TestMethod]
         public void TotalGoals()
@@ -21,17 +23,25 @@ namespace Football
         }
 
         [TestMethod]
-        public void LastMatch()
+        public void AddLastMatch()
         {
             Match lastMatch = new Match("U", "Brasov", 0, 0);
-            Match[] expectedValue = { new Match("U Cluj", "CFR", 2, 1), new Match("Petrolul", "Steaua", 0, 0), new Match("Botosani", "CSU", 3, 2), new Match("Dinamo", "ASA", 2, 1), new Match("Chiajna", "Astra", 0, 2), lastMatch };
+            Match[] expectedValue = { new Match("U Cluj", "CFR", 2, 1), new Match("Petrolul", "Steaua", 0, 0), new Match("Botosani", "CSU", 3, 2), new Match("Dinamo", "ASA", 2, 1), new Match("Chiajna", "Astra", 0, 2), new Match("U", "Brasov", 0, 0) };
             CollectionAssert.AreEqual(expectedValue, AddLastMatchOfTheRound(round, lastMatch));
         }
 
         [TestMethod]
         public void BestGoalsDifference()
         {
-            Assert.AreEqual(2, CalculateBestGoalDifference(round));
+            MatchWithBestDifference expectedValue = new MatchWithBestDifference("Astra", 4);
+            Assert.AreEqual(expectedValue, CalculateBestGoalDifference(round));
+        }
+
+        [TestMethod]
+        public void EliminateMatch()
+        {
+            Match[] expectedValue = { new Match("U Cluj", "CFR", 2, 1), new Match("Petrolul", "Steaua", 0, 0), new Match("Botosani", "CSU", 3, 2), new Match("Dinamo", "ASA", 2, 1) };
+            CollectionAssert.AreEqual(expectedValue, EliminateMatchWithBestGoalsDifference(round, 4));
         }
 
         public struct Match
@@ -46,6 +56,18 @@ namespace Football
                 this.guests = guests;
                 this.homeTeamGoals = homeTeamGoals;
                 this.awayTeamGoals = awayTeamGoals;
+            }
+        }
+
+        public struct MatchWithBestDifference
+        {
+            public string team;
+            public int index;
+            public MatchWithBestDifference(string team, int index)
+            {
+                this.team = team;
+                this.index = index;
+
             }
         }
         public static Match[] AddLastMatchOfTheRound(Match[] round, Match lastMatch)
@@ -75,19 +97,42 @@ namespace Football
             else return y;
         }
 
-        public static int CalculateBestGoalDifference(Match[] round)
+        public static MatchWithBestDifference CalculateBestGoalDifference(Match[] round)
         {
             int bestGoalsDifference = 0;
             int[] goalDifference = new int[round.Length];
-
+            int index = 0;
             for (int i = 0; i < round.Length; i++)
             {
                 goalDifference[i] = Math.Abs(round[i].homeTeamGoals - round[i].awayTeamGoals);
             }
             for (int i = 0; i < round.Length; i++)
-                bestGoalsDifference = MaxValue(goalDifference[i], bestGoalsDifference);
-            return bestGoalsDifference;
-        }
+            {
 
+                if (bestGoalsDifference < goalDifference[i])
+                {
+                    bestGoalsDifference = goalDifference[i];
+                    index = i;
+                }
+            }
+            MatchWithBestDifference bestDifference = new MatchWithBestDifference();
+            if (round[index].homeTeamGoals > round[index].awayTeamGoals)
+            {
+                bestDifference.team = round[index].hosts;
+            }
+            else
+                bestDifference.team = round[index].guests;
+            bestDifference.index = index;
+
+            return bestDifference;
+        }
+        public static Match[] EliminateMatchWithBestGoalsDifference(Match[] round, int index)
+        {
+
+            List<Match> newList = new List<Match>(round);
+            newList.RemoveAt(index);
+            return newList.ToArray();
+        }
     }
 }
+
