a7a1425061c572b96c47fd63881325c28c8263b3
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-10-23 16:34:19 +0300

    Hanoi optim version

--- Recursion/Recursion/Hanoi Towers.cs
+++ Recursion/Recursion/Hanoi Towers.cs
@@ -10,35 +10,29 @@ namespace Recursion
         [TestMethod]
         public void Hanoi()
         {
-            int[] startTower = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
-            int[] intermediateTower = new int[10];
-            int[] endTower = new int[10];
-            Assert.AreEqual(1023, MoveHanoiPlates(10, ref startTower, ref intermediateTower, ref endTower));
+            int[] startTower = new int[] { 3, 2, 1 };
+            int[] intermediateTower = new int[3];
+            int[] endTower = new int[3];
+            MoveHanoiPlates(3, ref startTower, ref intermediateTower, ref endTower);
+            Assert.AreEqual(7, MoveHanoiPlates(3, ref startTower, ref intermediateTower, ref endTower));
         }
 
         private int MoveHanoiPlates(int numberOfPlates, ref int[] startTower, ref int[] intermediateTower, ref int[] endTower)
         {
+            if (numberOfPlates == 0) return 0;
+            int numberOfMoves = MoveHanoiPlates(numberOfPlates - 1, ref startTower, ref endTower, ref intermediateTower);
             if (numberOfPlates == 1)
             {
-                endTower[0] = startTower[0];
-                for (int i = 0; i < startTower.Length - 1; i++)
-                {                                       
-                    startTower[i] = startTower[i + 1];
-                    
+                for (int i = 0; i < startTower.Length; i++)
+                {
+                    endTower[i] = startTower[i];
+                    startTower[i] = 0;
                 }
-                startTower[startTower.Length - 1] = 0;
-                return 1;
             }
-
-            else
-            {
-                int numberOfMoves = 0;
-                numberOfMoves = MoveHanoiPlates(numberOfPlates - 1, ref startTower, ref endTower, ref intermediateTower);
-                MoveHanoiPlates(1, ref startTower, ref intermediateTower, ref endTower);
-                numberOfMoves++;
-                numberOfMoves += MoveHanoiPlates(numberOfPlates - 1, ref intermediateTower, ref startTower, ref endTower);
-                return numberOfMoves;
-            }
+            numberOfMoves++;
+            numberOfMoves += MoveHanoiPlates(numberOfPlates - 1, ref intermediateTower, ref startTower, ref endTower);
+            return numberOfMoves;
         }
     }
 }
+
