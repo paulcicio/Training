bf6ba29b0f64ca0b4787fc895aebd74642eff2a3
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-10-26 11:28:01 +0200

    Added XGame project

---
+++ Recursion/Recursion/Xgame.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Recursion
+{
+    [TestClass]
+    public class Xgame
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(11, CalculateNumberOfWinningMoves(matrix[2][1], 4));
+        }
+
+        private int CalculateNumberOfWinningMoves(int[][] matrix, int n)
+        {
+            int count = 0;                                  
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[i - 1, j]==0 && matrix[i + 1, j]==0 &&
+                        matrix[i, j - 1]==0 && matrix[i, j + 1]==0)
+                    {
+                        matrix[i][j] = 1;
+                        CalculateNumberOfWinningMoves(matrix, n+1);
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
+
