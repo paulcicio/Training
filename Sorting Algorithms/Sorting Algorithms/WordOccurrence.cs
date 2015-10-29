8e78fddef7170cc553be048e0b4c8199581c1312
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-10-29 10:58:53 +0200

    Word Occurrence

--- Sorting Algorithms/Sorting Algorithms/WordOccurrence.cs
+++ Sorting Algorithms/Sorting Algorithms/WordOccurrence.cs
@@ -1,39 +1,92 @@
-﻿//using System;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
-//namespace Sorting_Algorithms
-//{
-//    [TestClass]
-//    public class WordOccurrence
-//    {
-//        [TestMethod]
-//        public void TestMethod1()
-//        {
+namespace Sorting_Algorithms
+{
+    [TestClass]
+    public class WordOccurrence
+    {
+        public struct Word
+        {
+            public string word;
+            public int counter;
+            public Word(string word, int counter)
+            {
+                this.word = word;
+                this.counter = counter;
+            }
+        }    
 
-//        }
-      
-//            private void SortWordsByOccurences(string text)
-//            {
-//                    string[] splitted = text.Split(' ');
-//                    int i, j, stanga, dreapta, m, aux;
-//                    for (i = 1; i <= splitted.Length; i++)
-//                    {
-//                        aux = a[i];
-//                        stanga = 1;
-//                        dreapta = splitted.Length;
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string text = "To be or not to be";
+            //List<Word> expectedValue = new List<Word>();
+            //List<Word> actualValue = new List<Word>();
+            Assert.AreEqual(1, CalculateNumberOfOccurrences(text));
+        }
 
-//                    }
-//                    while (stanga <= dreapta)
-//                    {
-//                        m = (stanga + dreapta) / 2;
-//                        if (a[stanga] > aux) dreapta = m - 1;
-//                        else stanga = m + 1;
-//                    }
-//                    for (j = i - 1; j >= stanga; j--)
-//                        a[j + 1] = a[j];
-//                    a[stanga] = aux;
-//                }
-//            }
-//        }
-    
+        public int CalculateNumberOfOccurrences(string text)
+        {
+            var set = new HashSet<string>(text.ToLower().Split(' '));
+            //for (int i = 0; i < splitted.Length; i++)
+            //{
+            //    int count = 1;
+            //    for (int j = i + 1; j < splitted.Length; j++)
+            //    {
+            //        if (splitted[i] == splitted[j])
+            //        {
+            //            count++;
+            //        }
+            //        else break;
+            //    }
+            //}
+            return 1;
+        }
+
+        public Word[] InsertionSort(Word[] array)
+        {           
+            int index, i, j;
+            for (i = 1; i < array.Length; i++)
+            {
+                index = array[i].counter;
+                j = i;
+                while ((j > 0) && (array[j - 1].counter > index))
+                {
+                    array[j].counter = array[j - 1].counter;
+                    j = j - 1;
+                }
+                array[j].counter = index;
+            }
+            return array;
+        }
+
+        public void BinarySearch(Word[] inputArray, int key, int min, int max)
+        {
+            //if (min > max)            
+            //    break;
+            
+            //else
+            //{
+                int mid = (min + max) / 2;
+                if (key == inputArray[mid].counter)
+                {
+                     ++mid;
+                }
+                else if (key < inputArray[mid].counter)
+                {
+                    BinarySearch(inputArray, key, min, mid - 1);
+                }
+                else
+                {
+                    BinarySearch(inputArray, key, mid + 1, max);
+                }
+            }
+        }
+    }
+
+
+
+
 
