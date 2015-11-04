c5a9aecc5c4b0bf263bd635a4976aac8a9890271
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-11-04 16:48:20 +0200

    Vector Methods

--- OOP/OOP/UnitTest1.cs
+++ OOP/OOP/UnitTest1.cs
@@ -5,8 +5,8 @@ using System.Collections;
 namespace OOP
 {
     [TestClass]
-    public class Vector: IEnumerable
-    {   
+    public class Vector : IEnumerable
+    {
         //Class variables               
 
         private object[] data;
@@ -14,25 +14,61 @@ namespace OOP
 
         //Class constructors
 
-        public Vector(object[] _data)
+        public Vector()
         {
-            data = _data;
+        }
+
+        public Vector(object[] data)
+        {
+            this.data = data;
             count = data.Length;
         }
 
         //Class Methods
 
-        public void Add(object obj)
+        public void Add(object[] obj)
         {
-            object newObject = new object[8];
             count++;
             Array.Resize(ref data, 2 * data.Length);
-            data[count-1] = newObject;
+            data[count - 1] = obj[0];
+
         }
-        public void Insert(int position, object obj) { }
-        public void Remove(object obj) { }
+        public void Insert(int position, object obj)
+        {
+            count++;
+            if (data.Length == 4)
+            {
+                Array.Resize(ref data, 2 * data.Length);
+            }
+                for (int i= data.Length - 2; i>=position; i--)
+                {
+                    data[i+1] = data[i];
+                }
+                data[position] = obj;
+            
+        }
+        public void Remove(object obj)
+        {          
+                       
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i] == obj)
+                
+                    data[i] = null;                    
+                
+            }
+            count--;
+            Array.Resize(ref data, data.Length - 1);
+        }
         public void Remove(int index) { }
-        public void Count() { }
+        public int Count()
+        {
+            return count;
+        }
+        public object[] GetData()
+        {
+            return data;
+        }
 
         public IEnumerator GetEnumerator()
         {
@@ -43,7 +79,7 @@ namespace OOP
                 //
                 // Returns:
                 //     The current element in the collection.
-            //object Current { get; }
+                //object Current { get; }
 
                 //
                 // Summary:
@@ -56,7 +92,7 @@ namespace OOP
                 // Exceptions:
                 //   T:System.InvalidOperationException:
                 //     The collection was modified after the enumerator was created.
-            //bool MoveNext();
+                //bool MoveNext();
                 //
                 // Summary:
                 //     Sets the enumerator to its initial position, which is before the first element
@@ -65,16 +101,51 @@ namespace OOP
                 // Exceptions:
                 //   T:System.InvalidOperationException:
                 //     The collection was modified after the enumerator was created.
-            //void Reset();
+                //void Reset();
             }
             throw new NotImplementedException();
         }
 
         [TestMethod]
-        public void TestMethod1()
+        public void AddNewObject()
         {
+            object[] localData = new object[4] { 3, 7, 5, 8 };
+            Vector vector = new Vector(localData);
+            object[] obj = new object[] { 2 };
+            object[] expectedValue = new object[] { 3, 7, 5, 8, 2, null, null, null };
+            vector.Add(obj);
+            CollectionAssert.AreEqual(expectedValue, vector.GetData());
+            Assert.AreEqual(5, vector.Count());
         }
 
+        [TestMethod]
+        public void CountObjects()
+        {
+            object[] localData = new object[4] { 3, 7, 5, 8 };
+            Vector vector = new Vector(localData);
+            Assert.AreEqual(4, vector.Count());
+        }
 
+        [TestMethod]
+        public void InsertNewObject()
+        {
+            object[] localData = new object[] { 3, 7, 5, 8 };
+            Vector vector = new Vector(localData);
+            object obj = 2 ;
+            object[] expectedValue = new object[] { 3, 7, 2, 5, 8, null, null, null };
+            vector.Insert(2, obj);
+            CollectionAssert.AreEqual(expectedValue, vector.GetData());            
+        }
+
+        [TestMethod]
+        public void RemoveObject()
+        {
+            object[] localData = new object[] { 3, 7, 2, 5, 8 };
+            Vector vector = new Vector(localData);
+            object obj = 2;
+            object[] expectedValue = new object[] { 3, 7, 5, 8, null, null, null };
+            vector.Remove(obj);
+            CollectionAssert.AreEqual(expectedValue, vector.GetData());
+        }
     }
 }
