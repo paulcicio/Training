4395104837920fee178d141b0027e33f94c20027
Paul Ciciovan <paul_ciciovan@outlook.com>
2015-11-06 10:22:11 +0200

    Added IEnumerator

---
+++ OOP/OOP/Enumerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Enumerator : IEnumerator
+    {
+        private Vector vector;
+        private int position = -1;
+        public Enumerator(Vector vector)
+        {
+            this.vector = vector;
+        }       
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < vector.Count());
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+        public object Current
+        {
+            get
+            {
+                return vector.GetElementAtIndex(position);
+            }
+        }        
+    }
+}
--- OOP/OOP/UnitTest1.cs
+++ OOP/OOP/UnitTest1.cs
@@ -16,27 +16,37 @@ namespace OOP
 
         public Vector()
         {
+            this.count = 0;
+            this.data = new object[8];
         }
 
         public Vector(object[] data)
         {
-            this.data = data;
             count = data.Length;
+            this.data = new object[count];
+            Array.Copy(data, this.data, count);
+            //this.data = data;
+
         }
 
         //Class Methods
 
-        public void Add(object[] obj)
+
+
+        public void Add(object obj)
         {
             count++;
-            Array.Resize(ref data, 2 * data.Length);
-            data[count - 1] = obj[0];
+            if (count >= data.Length)
+            {
+                Array.Resize(ref data, 2 * data.Length);
+            }
+            data[count - 1] = obj;
 
         }
         public void Insert(int position, object obj)
         {
             count++;
-            if (data.Length == 4)
+            if (count >= data.Length)
             {
                 Array.Resize(ref data, 2 * data.Length);
             }
@@ -50,39 +60,45 @@ namespace OOP
         public void Remove(object obj)
         {
             bool found = false;
-            for (int i = 0; i < data.Length-1; i++)
+            for (int i = 0; i < data.Length - 1; i++)
             {
                 if (data[i].Equals(obj))
-                {                    
+                {
                     found = true;
                 }
-                if (found == true)
+                if (found)
                 {
-                    data[i] = data[i + 1];                    
+                    data[i] = data[i + 1];
                 }
-                
             }
             data[count - 1] = null;
-            count--;           
+            count--;
         }
 
         public void Remove(int index)
         {
             bool found = false;
-            for (int i = 0; i < data.Length - 1; i++)
+            for (int i = index; i < data.Length - 1; i++)
             {
-                if (i== index)
+                if (i == index)
                 {
                     found = true;
                 }
-                if (found == true)
+                if (found)
                 {
                     data[i] = data[i + 1];
                 }
-
             }
             data[count - 1] = null;
             count--;
+        }
+
+        public object GetElementAtIndex(int index)
+        {
+            for (int i = 0; i < data.Length - 1; i++)
+                if (i == index)
+                    return data[i];
+            return null;
         }
 
         public int Count()
@@ -96,46 +112,31 @@ namespace OOP
 
         public IEnumerator GetEnumerator()
         {
-            {
-                //
-                // Summary:
-                //     Gets the current element in the collection.
-                //
-                // Returns:
-                //     The current element in the collection.
-                //object Current { get; }
+            return new Enumerator(this);
+        }
 
-                //
-                // Summary:
-                //     Advances the enumerator to the next element of the collection.
-                //
-                // Returns:
-                //     true if the enumerator was successfully advanced to the next element; false if
-                //     the enumerator has passed the end of the collection.
-                //
-                // Exceptions:
-                //   T:System.InvalidOperationException:
-                //     The collection was modified after the enumerator was created.
-                //bool MoveNext();
-                //
-                // Summary:
-                //     Sets the enumerator to its initial position, which is before the first element
-                //     in the collection.
-                //
-                // Exceptions:
-                //   T:System.InvalidOperationException:
-                //     The collection was modified after the enumerator was created.
-                //void Reset();
-            }
-            throw new NotImplementedException();
+        [TestMethod]
+        public void Enumerator()
+        {
+            object[] localData = new object[4] { 3, 7, 5, 8 };
+            Vector vector = new Vector(localData);
+            Enumerator enumerator = new Enumerator(vector);
+            enumerator.MoveNext();
+            object objCurrent = enumerator.Current;
+            enumerator.MoveNext();
+            enumerator.MoveNext();
+           Assert.AreEqual(true,  enumerator.MoveNext());
+            
+            object newObj = enumerator.Current;
+            Assert.AreEqual(3, objCurrent);
         }
 
         [TestMethod]
         public void AddNewObject()
         {
-            object[] localData = new object[4] { 3, 7, 5, 8 };
+            object[] localData = new object[] { 3, 7, 5, 8 };
             Vector vector = new Vector(localData);
-            object[] obj = new object[] { 2 };
+            object obj = 2;
             object[] expectedValue = new object[] { 3, 7, 5, 8, 2, null, null, null };
             vector.Add(obj);
             CollectionAssert.AreEqual(expectedValue, vector.GetData());
@@ -164,10 +165,10 @@ namespace OOP
         [TestMethod]
         public void RemoveObject()
         {
-            object[] localData = new object[] { 3, 7, 2, 5, 8};
+            object[] localData = new object[] { 3, 7, 2, 5, 8 };
             Vector vector = new Vector(localData);
             object obj = 2;
-            object[] expectedValue = new object[] { 3, 7, 5, 8, null};
+            object[] expectedValue = new object[] { 3, 7, 5, 8, null };
             vector.Remove(obj);
             CollectionAssert.AreEqual(expectedValue, vector.GetData());
         }
@@ -181,6 +182,15 @@ namespace OOP
             object[] expectedValue = new object[] { 3, 2, 5, 8, null };
             vector.Remove(index);
             CollectionAssert.AreEqual(expectedValue, vector.GetData());
+        }
+
+        [TestMethod]
+        public void GetElement()
+        {
+            object[] localData = new object[4] { 3, 7, 5, 8 };
+            Vector vector = new Vector(localData);
+            int position = 1;
+            Assert.AreEqual(7, vector.GetElementAtIndex(position));
         }
     }
 }
